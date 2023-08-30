25a89122b119c8ac8da02bc147752b3db931d7a9
TheTahaYaseen <110729233+TheTahaYaseen@users.noreply.github.com>
2023-08-30 22:02:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-import subprocess
-import os
-
-# Determine the path to the packages.toml file
-toml_path = os.path.join(os.path.dirname(__file__), 'packages.toml')
-
-# Install packages listed in packages.toml
-def install_packages_from_toml():
-    try:
-        subprocess.run(['pip', 'install', '-r', toml_path])
-        print("Packages installed successfully.")
-    except Exception as e:
-        print("Error installing packages:", e)
-install_packages_from_toml()
-
 from datetime import date, timedelta
 
 import sys
