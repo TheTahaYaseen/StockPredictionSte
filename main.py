c0a2196214fcc0cc732f47a05b5c17e5187ab779
TheTahaYaseen <110729233+TheTahaYaseen@users.noreply.github.com>
2023-09-01 01:55:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 from datetime import date, timedelta
 
-import sys
-import subprocess
-
 import streamlit as st
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
-
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
-
-response = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies').text
-
-soup = BeautifulSoup(response, "html.parser")
-
-tables = soup.find_all('table')
-
-company_symbols = []
-
-for row in tables[0].tbody.find_all("tr"):
-    col = row.find_all("td")
-    if (col != []):
-        symbol = col[0].text.strip().replace('\n','')
-        company_symbols.append(symbol)
-
-start_date = "2000-01-01"
 
 date_temp = date.today()
 date_of_today = date_temp.strftime("%Y-%m-%d")
+
+start_date = date_temp - timedelta(days=300)
+start_date = start_date.strftime("%Y-%m-%d")
 
 date_of_year_later = date_temp + timedelta(days=365)
 date_of_year_later = date_of_year_later.strftime("%Y-%m-%d")
@@ -44,11 +25,8 @@ date_of_week_later = date_of_week_later.strftime("%Y-%m-%d")
 date_of_day_later = date_temp + timedelta(days=1)
 date_of_day_later = date_of_day_later.strftime("%Y-%m-%d")
 
-
 st.title("The Stocks Prediction App")
-stocks = tuple(company_symbols)
-
-selected_stock = st.selectbox("Select Stock For Prediction", stocks)
+selected_stock = st.text_input(label="Stock Symbol: ", value="TSLA")
 
 @st.cache_data
 def load_data(ticker):
@@ -56,39 +34,49 @@ def load_data(ticker):
     data.reset_index(inplace=True)
     return data
 
-data = load_data(selected_stock)
+try:
+    data = load_data(selected_stock)
+    data_loaded = True
 
-df_training = data[["Date", "Close"]]
-df_training = df_training.rename(columns={"Date":"ds", "Close":"y"})
+except Exception:
+    st.write("Please Write A Valid Stock Symbol!")
+    data_loaded = False
 
-m = Prophet()
-m.fit(df_training)
+if data_loaded:
+    stock_price_of_today = str(data.iloc[-1]["Close"])
 
-future = m.make_future_dataframe(periods=375)
-forecast = m.predict(future)
+    data = data[["Date", "Close"]]
+    data = data.rename(columns={"Date":"ds", "Close":"y"})
 
-forecast['ds'] = pd.to_datetime(forecast['ds'])
+    m = Prophet(daily_seasonality=True, weekly_seasonality=True, yearly_seasonality=True)
+    m.fit(data)
 
-filtered_row = forecast[forecast['ds'] == date_of_year_later]
+    future = m.make_future_dataframe(periods=366)
+    forecast = m.predict(future)
 
-stock_price_of_year_later = filtered_row['yhat'].values[0]
+    forecast['ds'] = pd.to_datetime(forecast['ds'])
 
-filtered_row = forecast[forecast['ds'] == date_of_month_later]
+    filtered_row = forecast[forecast['ds'] == date_of_year_later]
 
-stock_price_of_month_later = filtered_row['yhat'].values[0]
+    stock_price_of_year_later = filtered_row['yhat'].values[0]
 
-filtered_row = forecast[forecast['ds'] == date_of_week_later]
+    filtered_row = forecast[forecast['ds'] == date_of_month_later]
 
-stock_price_of_week_later = filtered_row['yhat'].values[0]
+    stock_price_of_month_later = filtered_row['yhat'].values[0]
 
-filtered_row = forecast[forecast['ds'] == date_of_day_later]
+    filtered_row = forecast[forecast['ds'] == date_of_week_later]
 
-stock_price_of_day_later = filtered_row['yhat'].values[0]
+    stock_price_of_week_later = filtered_row['yhat'].values[0]
 
-st.write(f"Stock Forecast Till The Next Year: {stock_price_of_year_later}")
-st.write(f"Stock Forecast Till The Next Month: {stock_price_of_month_later}")
-st.write(f"Stock Forecast Till The Next Week: {stock_price_of_week_later}")
-st.write(f"Stock Forecast Till The Next Day: {stock_price_of_day_later}")
+    filtered_row = forecast[forecast['ds'] == date_of_day_later]
 
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+    stock_price_of_day_later = filtered_row['yhat'].values[0]
+
+    st.write(f"Stock Forecast Till The Next Year: {stock_price_of_year_later}")
+    st.write(f"Stock Forecast Till The Next Month: {stock_price_of_month_later}")
+    st.write(f"Stock Forecast Till The Next Week: {stock_price_of_week_later}")
+    st.write(f"Stock Forecast Till The Next Day: {stock_price_of_day_later}")
+    st.write(f"Stock Forecast Today: {stock_price_of_today}")
+
+    fig1 = plot_plotly(m, forecast)
+    st.plotly_chart(fig1)
